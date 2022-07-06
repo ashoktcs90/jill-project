532e4172dd55954fdd9e567c868f711c5918e0f2
thirumalesh <thirumaleshjupalli@gmail.com>
2022-07-06 16:33:09 +0530

    project jill

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import ProductListing from "./Containers/ProductListing";
+import Header from  "./Containers/Header/Header";
+import "./App.css";
+import {ProductDetails }from "./Containers/ProductDetails";
+import ProductDetails from "./Containers/ProductDetails/ProductDetails";
+// import { Component } from "react";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      
     </div>
+    </Router>
   );
 }
-
 export default App;
